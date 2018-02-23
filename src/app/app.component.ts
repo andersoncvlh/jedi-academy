dd9fcc28dbef822e0bcecc68dc2ff9e3b626061a
anderson.cvlh <anderson.cvlh@gmail.com>
2018-02-23 19:57:13 -0300

    Model student

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'jedi';
+  luke = {name: 'Luke', isJedi: true, temple: 'Coruscant'};
+  han = {name: 'Han Sole', isJedi: false};
+  leia = {name: 'Luke', isJedi: true, temple: 'Coruscant'};
 }
