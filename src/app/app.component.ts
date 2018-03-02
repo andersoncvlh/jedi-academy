f14fd612c65d056dd0cbeb7c025c7516717170f5
anderson.cvlh <anderson.cvlh@gmail.com>
2018-03-02 18:25:54 -0300

    ngIf and ngFor implement

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
 
+import { Student } from './student/student.model';
+
 @Component({
   selector: 'jedi-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  luke = {name: 'Luke', isJedi: true, temple: 'Coruscant'};
-  han = {name: 'Han Sole', isJedi: false};
-  leia = {name: 'Luke', isJedi: true, temple: 'Coruscant'};
+
+  students: Student[] = [
+    {name: 'Luke', isJedi: true, temple: 'Coruscant'},
+    {name: 'Han Sole', isJedi: false},
+    {name: 'Luke', isJedi: true, temple: 'Coruscant'}
+  ];
+
 }
